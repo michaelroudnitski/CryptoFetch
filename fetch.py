38ad1f4e8241ee9c736b8f8149af836f11b9f4f2
michaelroudnitski <michaelroudnitski@yahoo.ca>
2017-11-04 21:17:43 -0400

    try/except added

--- fetch.py
+++ fetch.py
@@ -10,11 +10,15 @@ urls = ['https://api.coinmarketcap.com/v1/ticker/bitcoin/',
         'https://api.coinmarketcap.com/v1/ticker/ethereum']
 
 def get_value(url):
-    rs = requests.get(url, timeout=5) # access page
+    try:
+        rs = requests.get(url, timeout=5) # access page
+    except requests.ConnectionError:
+        return "Failed to connect to coinmarketcap..."
+
     if rs != None: # if we successfully retreived data,
         data = rs.json()[0]
         return data["symbol"], data["price_usd"], data["percent_change_24h"]
     else: # return a message if we failed to connect to coinmarketcap
-        return "Failed to connect to coinmarketcap..."
+        return "Failed to retrieve data"
 
 print get_value(urls[1])
