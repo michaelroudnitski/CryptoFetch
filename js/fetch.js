4c8a6777869d01a4702ce03dad29debaf49c9d21
michaelroudnitski <michaelroudnitski@yahoo.ca>
2017-11-12 18:24:58 -0500

    dynamic cards
    - ability to create a uinique card for an arbitrary coin
    - cards display coin symbol, value & 24h change

--- js/fetch.js
+++ js/fetch.js
@@ -1,24 +1,39 @@
 // fetch.js
 
 $(function () {
-    class Coin {
-        constructor(symbol, price, change){
-            this.symbol = symbol;
-            this.price = price;
-            this.change = change;
-        }
+    var cmcAPI = "https://api.coinmarketcap.com/v1/ticker";
+    var selectedCoins = {"bitcoin":0, "ethereum":1, "dash":2, "ripple":3};
+
+    function appendText(symbol, value, change) {
+        $( "#container" ).append(
+            "<div class=\"card bg-light mb-3\">\
+                <div class=\"card-header\">\
+                    <h3>"+symbol+"\
+                    <button type=\"button\" class=\"close\" aria-label=\"Close\">\
+                        <span aria-hidden=\"true\">&times;</span>\
+                    </button></h3>\
+                </div>\
+                <div class=\"card-body\">\
+                    <h4 class=\"card-title\">$"+value+"</h4>\
+                    <p class=\"card-text\">"+change+"%</p>\
+                </div>\
+            </div>"
+        );
     }
-    // $.ajax({
-    //     method: "GET",
-    //     dataType: "JSON",
-    //     url: "https://api.coinmarketcap.com/v1/ticker/bitcoin/",
-    //     success: function(data) {
-    //         var data = data[0];
-    //         var btc = new Coin(data[0].symbol, data[0].price_usd, data[0].percent_change_24h);
-    //     }
-    // });
-    $.getJSON( "https://api.coinmarketcap.com/v1/ticker/bitcoin/", function(data) {
-        var data = data[0];
-        var btc = new Coin(data.symbol, data.price_usd, data.percent_change_24h);
-    });
+
+    function getData(url) {
+        $.getJSON( url, function(data) {    // get json from cmc api
+            $.each( data, function( i, coin ) {
+                if (coin.id in selectedCoins){  // if the coin we are checking is one of our coins of interest
+                    appendText(                 // create a card with the following data
+                        coin.symbol,
+                        parseFloat(coin.price_usd).toFixed(2),
+                        parseFloat(coin.percent_change_24h).toFixed(2)
+                    );
+                }
+            });
+        });
+    }
+
+    getData(cmcAPI);
 });
